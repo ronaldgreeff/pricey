5d044e3014da7aac99dc487d96a5fc1043c4488b
Ron G <ronald.greeff@gmail.com>
2020-10-03 07:48:25 +0100

    notes from nextjs

--- frontend/src/pages/api/test.js
+++ frontend/src/pages/api/test.js
@@ -30,3 +30,66 @@ export default (req, res) => {
 }
 
 // https://github.com/vercel/next.js/blob/canary/examples/api-routes-graphql/pages/api/graphql.js
+
+// -- index.js
+//
+// import useSWR from 'swr'
+//
+// const fetcher = (query) =>
+//   fetch('/api/graphql', {
+//     method: 'POST',
+//     headers: {
+//       'Content-type': 'application/json',
+//     },
+//     body: JSON.stringify({ query }),
+//   })
+//     .then((res) => res.json())
+//     .then((json) => json.data)
+//
+// export default function Index() {
+//   const { data, error } = useSWR('{ users { name } }', fetcher)
+//
+//   if (error) return <div>Failed to load</div>
+//   if (!data) return <div>Loading...</div>
+//
+//   const { users } = data
+//
+//   return (
+//     <div>
+//       {users.map((user, i) => (
+//         <div key={i}>{user.name}</div>
+//       ))}
+//     </div>
+//   )
+// }
+
+// -- graphql
+//
+// import { ApolloServer, gql } from 'apollo-server-micro'
+//
+// const typeDefs = gql`
+//   type Query {
+//     users: [User!]!
+//   }
+//   type User {
+//     name: String
+//   }
+// `
+//
+// const resolvers = {
+//   Query: {
+//     users(parent, args, context) {
+//       return [{ name: 'Nextjs' }]
+//     },
+//   },
+// }
+//
+// const apolloServer = new ApolloServer({ typeDefs, resolvers })
+//
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// }
+//
+// export default apolloServer.createHandler({ path: '/api/graphql' })
